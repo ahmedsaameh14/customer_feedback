4da317be6e927fb521fac675ac29afa87c43873a
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-03-02 16:48:49 +0200

    Install AI - Update Schema

--- model/feedback.model.js
+++ model/feedback.model.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const feedbackSchema = new mongoose.Schema({
-    text :{
-        type: String,
-        required : true,
-    }
-})
+  text: {
+    type: String,
+    required: true,
+  },
+  translatedText: {
+    type: String,
+  },
+  sentiment: {
+    type: String,
+  },
+  language: {
+    type: String,
+  },
+});
 
-module.exports = mongoose.model('feedback' , feedbackSchema);
+module.exports = mongoose.model("feedback", feedbackSchema);
